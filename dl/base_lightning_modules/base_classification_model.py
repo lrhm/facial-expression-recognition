952fb145a3bccb720a5472e56b0aa6867cd210da
lrhm <ilaimihar@gmail.com>
2022-05-15 12:31:05 +0200

    resnet model and base classification update

---
+++ dl/base_lightning_modules/base_classification_model.py
@@ -0,0 +1,98 @@
+from pytorch_lightning import LightningModule
+import torch as t
+import torch.nn.functional as F
+from argparse import Namespace
+
+import matplotlib.pyplot as plt
+import ipdb
+import os
+import torchmetrics
+from torchmetrics.classification import accuracy
+
+class BaseClassificationModel(LightningModule):
+    def __init__(self, params: Namespace):
+        super().__init__()
+        self.params = params
+        self.save_hyperparameters()
+        self.generator = t.nn.Sequential()
+        self.loss = t.nn.BCELoss()
+        self.val_accuracy = torchmetrics.Accuracy()
+        self.test_accuracy = torchmetrics.Accuracy()
+        self.num_classes = 7
+
+    def forward(self, z: t.Tensor) -> t.Tensor:
+        out = self.generator(z)
+        return out
+
+    def training_step(self, batch: tuple[t.Tensor, t.Tensor], batch_idx: int):
+        x, y = batch
+        y_pred = self(x)
+        loss = self.loss(y_pred, y)
+        return {"loss": loss}
+
+    def validation_epoch_end(self, outputs):
+        acc = self.val_accuracy.compute()
+        self.log("accuracy", acc, prog_bar=True)
+        self.log("val_loss", 1 - acc, prog_bar=True)
+        self.val_accuracy.reset()
+        t.save(
+            self.state_dict(), os.path.join(self.params.save_path, "checkpoint.ckpt"),
+        )
+        return {"val_loss": acc}
+
+    def training_epoch_end(self, outputs):
+        avg_loss = t.stack([x["loss"] for x in outputs]).mean()
+
+    def validation_step(self, batch: tuple[t.Tensor, t.Tensor], batch_idx: int):
+        x, y = batch
+        if batch_idx == 0:
+            pass
+        pred_y = self(x)
+        maxes = t.argmax(pred_y, dim=1)
+        pred_y = t.eye(self.num_classes)[maxes].to(self.device)
+        y = y.int()
+        self.val_accuracy.update(pred_y, y)
+
+    def test_step(self, batch: tuple[t.Tensor, t.Tensor], batch_idx: int):
+        x, y = batch
+        if batch_idx == 0:
+            pass
+        pred_y = self(x)
+        maxes = t.argmax(pred_y, dim=1) # one hot encoding after softmax to predict one class not a probability
+        pred_y = t.eye(self.num_classes)[maxes].to(self.device)
+        y = y.int()
+        self.test_accuracy.update(pred_y, y)
+        return 
+
+    def test_epoch_end(self, outputs):
+        accuracy = self.test_accuracy.compute()
+        self.test_accuracy.reset()
+        test_metrics = {
+            "accuracy": accuracy,
+        }
+        test_metrics = {k: v for k, v in test_metrics.items()}
+        self.log("test_performance", test_metrics, prog_bar=True)
+
+    def configure_optimizers(self):
+        lr = self.params.lr
+        b1 = self.params.b1
+        b2 = self.params.b2
+
+        optimizer = t.optim.Adam(
+            self.generator.parameters(), lr=lr, betas=(b1, b2)
+        )
+        
+        scheduler = t.optim.lr_scheduler.ReduceLROnPlateau(
+            optimizer,
+            mode="min",
+            factor=0.1,
+            patience=self.params.reduce_lr_on_plateau_patience,
+            min_lr=1e-6,
+            verbose=True,
+        )
+
+        return {
+            "optimizer": optimizer,
+            "lr_scheduler": scheduler,
+            "monitor": "val_loss",
+        }
---
+++ dl/custom_fer_loaders.py
@@ -0,0 +1,105 @@
+import csv
+import pathlib
+from typing import Any, Callable, Optional, Tuple
+
+import torch
+from PIL import Image
+
+
+from torchvision.datasets.utils import verify_str_arg, check_integrity
+from torchvision.datasets.vision import VisionDataset
+from torchvision import datasets, transforms
+
+import ipdb
+
+
+class FER2013(VisionDataset):
+    """`FER2013
+    <https://www.kaggle.com/c/challenges-in-representation-learning-facial-expression-recognition-challenge>`_ Dataset.
+
+    Args:
+        root (string): Root directory of dataset where directory
+            ``root/fer2013`` exists.
+        split (string, optional): The dataset split, supports ``"train"`` (default), or ``"test"``.
+        transform (callable, optional): A function/transform that takes in an PIL image and returns a transformed
+            version. E.g, ``transforms.RandomCrop``
+        target_transform (callable, optional): A function/transform that takes in the target and transforms it.
+    """
+
+    _RESOURCES = {
+        "fer": ("train.csv", "f8428a1edbd21e88f42c73edd2a14f95"),
+    }
+
+    def __init__(
+        self,
+        data_location: str = None,
+        train: bool = True,
+        transform: Optional[Callable] = None,
+        target_transform: Optional[Callable] = None,
+    ) -> None:
+
+        super().__init__(data_location, transform=transform, target_transform=target_transform)
+
+        base_folder = data_location
+        file_name, md5 = self._RESOURCES["fer"]
+        data_file = data_location #+ file_name
+
+        
+        trsfm = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                # transforms.RandomHorizontalFlip(),
+                transforms.RandomAffine(degrees=10),
+                # transforms.Normalize((0.1307,), (0.3081,)),
+            ]
+        )
+        self.transform = trsfm
+
+        if not check_integrity(str(data_file), md5=md5):
+            raise RuntimeError(
+                f"{file_name} not found in {base_folder} or corrupted. "
+                f"You can download it from "
+                f"https://www.kaggle.com/c/challenges-in-representation-learning-facial-expression-recognition-challenge"
+            )
+
+        with open(data_file, "r", newline="") as file:
+            self._samples = [
+                (
+                    torch.tensor(
+                        [int(idx) for idx in row["pixels"].split()], dtype=torch.uint8
+                    ).reshape(48, 48),
+                    int(row["emotion"]) if "emotion" in row else None,
+                )
+                for row in csv.DictReader(file)
+            ]
+
+        test_percentage = 0.1
+        if train:
+            self._split = "train"
+            self._samples = self._samples[: -int(len(self._samples) * test_percentage)]
+        else:
+            self._split = "test"
+            self._samples = self._samples[-int(len(self._samples) * test_percentage) :]
+
+        # ipdb.set_trace()
+
+    def __len__(self) -> int:
+        return len(self._samples)
+
+    def __getitem__(self, idx: int) -> Tuple[Any, Any]:
+        image_tensor, target = self._samples[idx]
+        image = Image.fromarray(image_tensor.numpy())
+
+        if self.transform is not None:
+            image = self.transform(image)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        # one hot encoding
+        target = torch.eye(7)[target]
+
+        return image, target
+
+    def extra_repr(self) -> str:
+        return f"split={self._split}"
